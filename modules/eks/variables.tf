0fd70f1aeafd41151a6fbdb90436c37cd54e589a
Mohammad Anas <anas4u02@gmail.com>
2026-02-14 00:44:17 +0530

    pushed uncommitted changes

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -34,3 +34,20 @@ variable "developer_names" {
 variable "user_arns" {
   type = map(string)
 }
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
+
+variable "user_namespaces" {
+  type = list(string)
+  default = [ "jenkins" ]
+}
