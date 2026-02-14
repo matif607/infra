dff04d256b331ebb7776456a1bbd57855db13411
Mohammad Anas <anas4u02@gmail.com>
2026-02-14 12:23:59 +0530

    removed hardcoded bucket

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,5 @@
 terraform {
   backend "s3" {
-    bucket = "my1stbucketforterraform"
-    key = "dev/infra/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform"
     encrypt = true
   }
 }
